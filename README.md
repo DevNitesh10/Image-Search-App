22e9705c1256c18d775503ebd7ed611c35869b41
DevNitesh10 <princenitesh325@gmail.com>
2023-08-09 14:59:38 +0545

    Comments Added and ReadMe.md File Updated

--- README.md
+++ README.md
@@ -1,8 +1,59 @@
-# React + Vite
+# Image Search App
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+This is a simple React application that allows users to search for images using the Unsplash API and display the results in a grid. Users can also filter images by predefined categories and navigate through the pages of search results.
 
-Currently, two official plugins are available:
+## Table of Contents
 
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+- [Features](#features)
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contributing](#contributing)
+- [License](#license)
+
+## Features
+
+- Search for images based on a query.
+- Filter images by predefined categories.
+- Pagination for navigating through search results.
+- Responsive design for various screen sizes.
+- Error handling for failed image fetch requests.
+
+## Installation
+
+1. Clone the repository:
+git clone https://github.com/your-username/image-search-app.git
+
+2. Navigate to the project directory:
+cd image-search-app
+
+3. Install the dependencies:
+npm install
+
+4. Obtain an Unsplash API access key by signing up on the Unsplash Developer Portal.
+
+5. Create a .env.local file in the root directory and add your Unsplash API access key:
+VITE_API_KEY=your-api-key
+
+6. Start the development server:
+npm run dev
+
+7. Open your browser and go to http://localhost:5173 to view the app.
+5173 is the port that I used. You can use the port shown at your terminal after you type the command npm run dev.
+
+## Usage
+Enter a search query in the input field and press Enter or click the search button.
+Click on the predefined category buttons to filter images by category.
+Use the "Previous" and "Next" buttons to navigate through the pages of search results.
+Hover over images to see a subtle animation.
+
+## Contributing
+Contributions are welcome! If you find a bug or have a suggestion for improvements, feel free to open an issue or create a pull request.
+
+Fork the repository.
+Create a new branch: git checkout -b feature/your-feature-name.
+Make your changes and commit them: git commit -m 'Add some feature'.
+Push to the branch: git push origin feature/your-feature-name.
+Create a pull request describing your changes.
+
+## License
+This project is licensed under the MIT License.
--- src/App.jsx
+++ src/App.jsx
@@ -8,12 +8,16 @@ const IMAGES_PER_PAGE = 36;
 
 
 function App() {  
+  // Ref to access the search input element
   const searchInput = useRef(null);
+
+   // State variables
   const [images, setImages] = useState([]);
   const [page, setPage] = useState(1);
   const [totalPages, setTotalPages] = useState(0);
   const [errorMsg, setErrorMsg] = useState('');
 
+  // Function to fetch images from the API
   const fetchImages = useCallback(async () => {
     try {
       if (searchInput.current.value) {
@@ -25,8 +29,8 @@ function App() {
             import.meta.env.VITE_API_KEY
           }`
         );
-        setImages(data.results);
-        setTotalPages(data.total_pages);
+        setImages(data.results); // Update images state with fetched data
+        setTotalPages(data.total_pages); // Update total pages based on API response
       }
     } catch (error) {
       setErrorMsg('Error fetching images. Try again later.');
@@ -34,44 +38,56 @@ function App() {
     }
   }, [page]);
 
+  // Fetch images on component mount and whenever the fetchImages function changes
   useEffect(() =>  {
     fetchImages();
   }, [fetchImages]) ;  
   
+  // Function to reset search and fetch new images
   const resetSearch = () => {
-    setPage(1);
-    fetchImages();
+    setPage(1); // Reset page to 1
+    fetchImages(); // Fetch new images based on the reset search
   }
 
+   // Function to handle search form submission
   const handleSearch = (event) => {
-    event.preventDefault(); //prevents page reload
-    console.log(searchInput.current.value); //points to input field we assigned to ref.
-    resetSearch();
+    event.preventDefault(); // Prevents page reload on form submission
+    resetSearch(); // Reset search and fetch new images
   };
 
+   // Function to handle category selection
   const handleSelection = (selection) => {
-    searchInput.current.value = selection;
-    resetSearch();
+    searchInput.current.value = selection; // Set search input value to the selected category
+    resetSearch(); // Reset search and fetch new images based on the selected category
   };
-  console.log('Page', page)
+
   return (
     <div className='container'>
     <h1 className='title'>Image Search</h1>
+
+    {/* Display error message if present */}
     {setErrorMsg && <p className='error-msg'>{errorMsg}</p>}
+
+    {/* Search Section */}
     <div className="search-section">
       <Form onSubmit={handleSearch}>
       <Form.Control type="search"
        placeholder="Type Something to Search..."
        className='search-input'
-       ref={searchInput} />
+       ref={searchInput} // Assign the search input element to the ref
+        /> 
       </Form>
     </div>
+
+     {/* Filters Section */}
     <div className="filters">
       <div onClick={() => handleSelection('nature')}>Nature</div>
       <div onClick={() => handleSelection('birds')}>Birds</div>
       <div onClick={() => handleSelection('cats')}>Cats</div>
       <div onClick={() => handleSelection('shoes')}>Shoes</div>
     </div>
+
+    {/* Images Section */}
     <div className="images">
     {images.map((image) => (
           <img
@@ -83,6 +99,7 @@ function App() {
         ))}
     </div>
 
+     {/* Pagination Buttons */}
     <div className='buttons'>
         {page > 1 && (
           <Button onClick={() => setPage(page - 1)}>Previous</Button>
